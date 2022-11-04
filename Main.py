ed0693412403aca321e7a2eb3dc2745dcccd9a4d
URK21CO3026 <110015042+urk21co3026@users.noreply.github.com>
2022-11-04 19:20:19 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 class BinaryTreeNode:
-    def _init_(self, data):
+    def __init__(self, data):
         self.data = data
-        self.left_child = None
-        self.right_child = None
+        self.left = None
+        self.right = None
 
 
 def insert(root, new_value) -> BinaryTreeNode:
@@ -12,46 +12,34 @@ def insert(root, new_value) -> BinaryTreeNode:
         Finally, return the root.
         """
     # Write your code here
-    if not root:
+    if root == None:
         root = BinaryTreeNode(new_value)
-        return root
-    if new_value < root.data:
-         if root.left_child:
-            insert(root.left_child, new_value)
-         else:
-            root.left_child = BinaryTreeNode(new_value)
-    else:
-        if root.right_child:
-            insert(root.right_child, new_value)
-        else:
-            root.right_child = BinaryTreeNode(new_value)
+    elif new_value < root.data:
+        root.left = insert(root.left, new_value)
+    else: #new_value >= root.data:
+        root.right = insert(root.right, new_value)
+    return root        
 
 
 def inorder(root) -> None:
-    # Write your code here
-    if root is None:
-        return
-    inorder(root.left_child)
-    print(root.data, end = " ")
-    inorder(root.right_child)
+    if root: 
+        inorder(root.left)
+        print(root.data, end = " ")
+        inorder(root.right)
 
 
 def preorder(root) -> None:
-    # Write your code here
-    if root is None:
-        return
-    print(root.data, end = " ")
-    preorder(root.left_child)
-    preorder(root.right_child)
+    if root:
+        print(root.data, end = " ")
+        preorder(root.left)
+        preorder(root.right)
 
 
 def postorder(root) -> None:
-    # Write your code here
-    if root is None:
-        return
-    postorder(root.left_child)
-    postorder(root.right_child)
-    print(root.data, end = " ")
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.data, end = " ")
 
 
 # Do not change the following code
